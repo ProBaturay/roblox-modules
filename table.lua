568bd8f2f43f53a7a8fdcf34d2495b55afe31c62
ProBaturay <99765925+ProBaturay@users.noreply.github.com>
2023-06-07 19:34:59 +0300

    Update table.lua

--- table.lua
+++ table.lua
@@ -14,10 +14,30 @@
 
 local deprecatedFunctions = {"foreach", "foreachi", "getn"}
 
+local function count(tab: {any}): number
+	local increment = 0
+
+	for i, v in tab do
+		increment += 1
+	end
+
+	return increment
+end
+
+local metatable = {
+	__len = function(tab)
+		return count(tab)
+	end,
+}
+
 local renewedTableLibrary = {}
 
 function renewedTableLibrary:GetDeprecated()
 	return deprecatedFunctions
+end
+
+function renewedTableLibrary:AddMetatable(tab: {any})
+	return setmetatable(tab, metatable)
 end
 
 function renewedTableLibrary.pack(...: any)
@@ -32,6 +52,14 @@ function renewedTableLibrary.find<a>(tab: {any}, arg: a): a?
 	end
 	
 	return nil
+end
+
+function renewedTableLibrary.getn(tab)
+	return count(tab)
+end
+
+function renewedTableLibrary.maxn(tab)
+	return count(tab)
 end
 
 for i, v in table :: {any} do
